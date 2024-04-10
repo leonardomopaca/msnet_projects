6a9153551715113ee7f1fe00a3456e020abee029
Leonardo Mopaca <leonardomopaca@gmail.com>
2024-04-10 03:40:27 -0500

    ini

---
+++ WebApplication1/Controller/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controller;
+
+public class UserController : Microsoft.AspNetCore.Mvc.Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
